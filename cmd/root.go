ffd68093d5cd990779fb3432d00a7e6b0d857b70
yf L <lyf571321556@gmail.com>
2021-07-16 17:17:11 +0800

    add: 加入定时执行任务以及命令行启动

---
+++ cmd/root.go
@@ -0,0 +1,59 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"os"
+
+	"github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+var rootCmd = &cobra.Command{
+	Use:   "manhour-reminder",
+	Short: "",
+	Long:  ``,
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/config.yaml)")
+	rootCmd.PersistentFlags().BoolP("daemon", "d", false, "service run on background?")
+	rootCmd.PersistentFlags().StringP("user", "u", "", "ONES的用户名")
+	rootCmd.PersistentFlags().StringP("password", "p", "false", "ONES的密码")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".manhour-reminder" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName("config")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
